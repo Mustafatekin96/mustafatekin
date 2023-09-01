42446873bccfb67597ab8acdf09b1609facb455b
Mustafatekin96 <125879096+Mustafatekin96@users.noreply.github.com>
2023-09-01 04:48:40 +0300

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+
+# Streamlit başlığı
+st.title("Veri Tabanlı Tahmin Uygulaması")
+
+# Veri girişleri için text alanları oluşturma
+st.sidebar.header("Veri Girişleri")
+input_1 = st.sidebar.text_input("Giriş 1", "Değer 1")
+input_2 = st.sidebar.text_input("Giriş 2", "Değer 2")
+input_3 = st.sidebar.text_input("Giriş 3", "Değer 3")
+input_4 = st.sidebar.text_input("Giriş 4", "Değer 4")
+input_5 = st.sidebar.text_input("Giriş 5", "Değer 5")
+
+# Düğmeye tıklanınca tahmin yapma işlemi
+if st.sidebar.button("Tahmin Yap"):
+    # Gerçek bir model kullanmak isterseniz, modeli yükleyip tahmin işlemini burada gerçekleştirebilirsiniz
+    # Örnek basit tahmin:
+    prediction = "Tahmin Yok"
+    if input_1 and input_2 and input_3 and input_4 and input_5:
+        prediction = "Örnek Tahmin Sonucu"
+
+    # Tahmin sonucunu gösterme
+    st.write("Tahmin Sonucu:=1")
+    st.write(prediction)
