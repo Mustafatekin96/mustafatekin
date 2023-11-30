80d3c7a3de19da60270f3e1e81f8b96971c801a1
Mustafatekin96 <125879096+Mustafatekin96@users.noreply.github.com>
2023-11-30 11:15:30 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,3 +4,4 @@ import pandas as pd
 github_raw_url = 'https://github.com/Mustafatekin96/mustafatekin/blob/5a2d820e3f9649f163350d9eabad035f487872c4/.devcontainer/machine.csv'
 
 df = pd.read_csv(github_raw_url)
+st.dataframe(df)
