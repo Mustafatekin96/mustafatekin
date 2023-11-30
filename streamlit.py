d626f0d4cb58866de5470cefd97c4954a1ce0584
Mustafatekin96 <125879096+Mustafatekin96@users.noreply.github.com>
2023-11-30 11:00:56 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-github_url = 'https://raw.githubusercontent.com/username/repo/main/data.csv'
+github_url = 'https://github.com/Mustafatekin96/mustafatekin/blob/441c75177f62f6514c836a915076183ce4130961/makine%20saat-eyl%C3%BCl-ekim.xlsx'
 
-# Pandas DataFrame oluşturma
-df = pd.read_csv(github_url)
+df = pd.read_excel(github_url)
