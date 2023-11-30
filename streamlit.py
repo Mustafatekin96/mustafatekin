99c5e0e202762b805055382505e54c14d7519346
Mustafatekin96 <125879096+Mustafatekin96@users.noreply.github.com>
2023-11-30 11:05:01 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-github_url = 'https://github.com/Mustafatekin96/mustafatekin/blob/441c75177f62f6514c836a915076183ce4130961/makine%20saat-eyl%C3%BCl-ekim.xlsx'
+github_url = 'https://github.com/Mustafatekin96/mustafatekin/blob/13c37485be03ad40b3f4ad1ca4408171bfe2c90f/makine%20saat-eyl%C3%BCl-ekim.csv'
 
 df = pd.read_csv(github_url)
