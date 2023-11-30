eda52a502b98f3a16cb9968937804c0679a5f062
Mustafatekin96 <125879096+Mustafatekin96@users.noreply.github.com>
2023-11-30 11:47:26 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 
 github_raw_url = 'https://github.com/Mustafatekin96/mustafatekin/blob/e71f0592998c28cbcd645aa1e2de328d34a3d22b/customers.csv'
-df = pd.read_csv(github_raw_url)
+
+df = pd.read_csv(github_raw_url, sep='\t')  # Sütunlar sekme (tab) ile ayrılmış
 
 st.dataframe(df)
