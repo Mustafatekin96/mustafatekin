a7f94879c64b111f56f988733768d3d538f56955
Mustafatekin96 <125879096+Mustafatekin96@users.noreply.github.com>
2023-11-30 13:33:58 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import pandas as pd
-import openpyxl
+
 # Streamlit uygulamasını başlat
 st.title("Excel Okuma Uygulaması")
 
 # Dosya seçme aracı ekleyin
-uploaded_file = st.file_uploader("Excel dosyanızı yükleyin", type=["xlsx", "xls"])
+uploaded_file = st.file_uploader("Excel dosyanızı yükleyin", type=["xlsx", "xls","csv"])
 
 # Dosya yüklendiyse
 if uploaded_file is not None:
     # Excel dosyasını oku
-    df = pd.read_excel(uploaded_file)
+    df = pd.read_csv(uploaded_file)
 
     # Veri çerçevesini görüntüle
     st.write("Yüklenen Excel Dosyası:")
