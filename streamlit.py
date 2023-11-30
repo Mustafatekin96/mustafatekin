95aeed7df0c056a1cf10d965e931c6a31fa187a2
Mustafatekin96 <125879096+Mustafatekin96@users.noreply.github.com>
2023-11-30 11:24:41 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 
 github_raw_url = 'https://github.com/Mustafatekin96/mustafatekin/blob/5a2d820e3f9649f163350d9eabad035f487872c4/.devcontainer/machine.csv'
+try:
+    df = pd.read_csv(github_raw_url)
+except pd.errors.ParserError as e:
+    st.error(f"Error reading CSV: {e}")
 
 
-df = pd.read_csv(github_raw_url, sep=',') 
 st.dataframe(df.head())
