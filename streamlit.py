576db6ea76035655b75bad7f507f7d25707261b2
Mustafatekin96 <125879096+Mustafatekin96@users.noreply.github.com>
2023-11-30 11:54:29 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,4 @@
 import streamlit as st
 import pandas as pd
-df = pd.read_csv("https://github.com/Mustafatekin96/mustafatekin/blob/e71f0592998c28cbcd645aa1e2de328d34a3d22b/customers.csv", sep=';')  # Sütunlar sekme (tab) ile ayrılmış
 
-st.dataframe(df)
+
