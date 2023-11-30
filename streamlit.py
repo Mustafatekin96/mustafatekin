a32b3c26ab079554ff5920c69bb43e6ddb4dde8b
Mustafatekin96 <125879096+Mustafatekin96@users.noreply.github.com>
2023-11-30 11:21:35 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,11 +3,6 @@ import pandas as pd
 
 github_raw_url = 'https://github.com/Mustafatekin96/mustafatekin/blob/5a2d820e3f9649f163350d9eabad035f487872c4/.devcontainer/machine.csv'
 
-
-with st.echo():
-    try:
-        df = pd.read_csv(github_raw_url)
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-df = df.dropna()
+df = pd.read_csv(github_raw_url)
+ 
 st.dataframe(df.head())
